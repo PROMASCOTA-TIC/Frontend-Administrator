5af92ce593760b94f65d6a22e519f6bbcb68f55a
Rafaela Robalino <rafaela.robalino@epn.edu.ec>
2024-10-14 18:00:25 -0500

    feat: Añadir pie de página y archivo general.css

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,8 +1,10 @@
+import PieDePagina from "@/components/ui/footer/PieDePagina";
 
 export default function Home() {
   return (
     <div>
       <h1>Hola mundo</h1>
+      <PieDePagina />
     </div>
   );
 }
