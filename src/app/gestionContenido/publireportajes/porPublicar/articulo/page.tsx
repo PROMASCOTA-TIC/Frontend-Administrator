3094e759f8cafb7510bede77143ecfa317786a50
Rafaela Robalino <rafaela.robalino@epn.edu.ec>
2024-10-24 19:30:47 -0500

    feat: Añadir seccion de publireportajes completa

---
+++ src/app/gestionContenido/publireportajes/porPublicar/articulo/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import PieDePagina from "@/components/ui/footer/PieDePagina";
+import { Box } from "@mui/material";
+import Btn_PorPublicar from "@/components/gestionContenido/barraBotones/Btn_PorPublicar";
+import PR_Articulo from "@/components/gestionContenido/PR_Articulo";
+
+const page = () => {
+    return (
+        <div>
+            <Box
+                className='flex-column'
+                sx={{
+                    padding: '34px 55px',
+                    gap: '21px'
+                }}
+            >
+                <h1 className='h1-bold txtcolor-primary'>Categoria</h1>
+                <PR_Articulo />
+                <div style={{ paddingTop: '34px' }}>
+                    <Btn_PorPublicar />
+                </div>
+            </Box>
+            <PieDePagina />
+        </div>
+    );
+}
+
+export default page
