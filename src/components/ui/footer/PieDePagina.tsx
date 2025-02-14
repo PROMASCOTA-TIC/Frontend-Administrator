da90acbdcaeafd8a39aaa6c4a7060027a7a54cab
Jean Villacis <jean.villacis@epn.edu.ec>
2025-02-14 15:22:45 -0500

    feat: Actualizar enlaces de redes sociales en el pie de página

--- src/components/ui/footer/PieDePagina.tsx
+++ src/components/ui/footer/PieDePagina.tsx
@@ -37,13 +37,13 @@ const PieDePagina: React.FC<PieDePaginaProps> = ({ isOpen }) => {
             <div>
                 <p className='n-bold'>Redes Sociales</p>
                 <div className='flex-around'>
-                    <IconButton href="https://www.facebook.com" target="_blank" color='inherit'>
+                    <IconButton href="https://www.facebook.com/share/18kPrQkn6p/?mibextid=wwXIfr" target="_blank" color='inherit'>
                         <Facebook />
                     </IconButton>
-                    <IconButton href="https://www.instagram.com" target="_blank" color='inherit'>
+                    <IconButton href="https://www.instagram.com/pro_maskota?igsh=MWJzcDh1ZXNwbzltZA==" target="_blank" color='inherit'>
                         <Instagram />
                     </IconButton>
-                    <IconButton href="https://www.tiktok.com" target="_blank" color='inherit'>
+                    <IconButton href="https://x.com/promaskota?s=21&t=zNTMFwVc03kXulFPEG6ZGg" target="_blank" color='inherit'>
                         <X />
                     </IconButton>
                 </div>
